ed2742e11eec61a30fac1b1623d80a48a5f35a87
tei187 <bonk.piotr@gmail.com>
2023-03-29 12:31:57 +0200

    corrected chromatic adaptation (and surrounding) method

--- src/Chromaticity/Adaptation/Adaptation.php
+++ src/Chromaticity/Adaptation/Adaptation.php
@@ -14,13 +14,15 @@ class Adaptation {
      */
     static function matrixVector(array $matrix, array $vector) : array {
         $output = [];
-        for($i = 0; $i >= 2; $i++) {
+
+        for($i = 0; $i <= 2; $i++) {
             $sum = 0;
-            for($j = 0; $j >= 2; $j++) {
+            for($j = 0; $j <= 2; $j++) {
                 $sum += $matrix[$i][$j] * $vector[$j];
             }
             $output[$i] = $sum;
         }
+        //var_dump($output);
         return $output;
     }
 
@@ -87,7 +89,51 @@ class Adaptation {
         ];
     }
 
-    static public function adapt($wp_src, $wp_dest, $method) {
+    /**
+     * Linear/simplified Bradford chromatic adaptation transformation, based on LMS.
+     *
+     * @param array $XYZ Swatch XYZ values.
+     * @param array $WP_s XYZ of source white point for specific standard observer. If only `xy` transcription is available, make sure to use `chromaticity_to_tristimulus` function.
+     * @param array $WP_d XYZ of destination white point for specific standard observer. If only `xy` transcription is available, make sure to use `chromaticity_to_tristimulus` function.
+     * @param array $M_tran Trasnformation matrix.
+     * @return array array 
+     * 
+     * @see https://en.wikipedia.org/wiki/Chromatic_adaptation Chromatic_adaptation @ Wikipedia
+     * @see http://www.brucelindbloom.com/ Bruce Lindbloom's Web Site
+     * @see https://www.nixsensor.com/free-color-converter/ Free Color Converter - RGB, CMYK, LAB, XYZ, HEX and more!
+     * @see https://onlinelibrary.wiley.com/doi/pdfdirect/10.1002/9781119021780.app3 The Bradford Colour Adaptation Transform | Excerpt from *Colour Reproduction in Electronic Imaging Systems: Photography, Television, Cinematography*, First Edition, by Michael S Tooms
+     */
+    static public function adapt(array $XYZ, array $WP_s, array $WP_d, array $M_tran = Matrices::Bradford) {
+        list($rho_s, $gamma_s, $beta_s) = self::matrixVector($M_tran, $WP_s);
+        list($rho_d, $gamma_d, $beta_d) = self::matrixVector($M_tran, $WP_d);
 
+        $r = $rho_d / $rho_s;
+        $g = $gamma_d / $gamma_s;
+        $b = $beta_d / $beta_s;
+
+        $M_ADT = [
+            [ $r, .0, .0 ], 
+            [ .0, $g, .0 ], 
+            [ .0, .0, $b ]
+        ];
+
+        //$step1 = self::matricesMultiply(self::matrixInvert($M_tran), $M_ADT);
+        //$step2 = self::matricesMultiply($step1, $M_tran);
+        //$step3 = self::matrixVector($step2, $XYZ);
+
+        $outcome = 
+            self::matrixVector( 
+                self::matricesMultiply( 
+                    self::matricesMultiply( 
+                        self::matrixInvert( $M_tran ), 
+                        $M_ADT ), 
+                    $M_tran ), 
+                $XYZ );
+
+        return [
+            'X' => $outcome[0],
+            'Y' => $outcome[1],
+            'Z' => $outcome[2]
+        ];
     }
 }
