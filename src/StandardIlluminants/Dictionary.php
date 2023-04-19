361d4aa2741980e4e75f3290c4b7f902503b0de4
tei187 <bonk.piotr@gmail.com>
2023-04-19 13:42:12 +0200

    Standard Illuminants Dictionary methods added

--- src/StandardIlluminants/Dictionary.php
+++ src/StandardIlluminants/Dictionary.php
@@ -2,6 +2,8 @@
 
 namespace tei187\ColorTools\StandardIlluminants;
 
+use tei187\ColorTools\Conversion\Convert;
+
 class Dictionary {
     /**
      * Array holding information about defined standard illuminants names.
@@ -39,5 +41,69 @@ class Dictionary {
             $as !== ""
                 ? $as
                 : false;
+    }
+
+    private static function _checkString(string $s) : array {
+        $out = [];
+        $e = explode("|", $s);
+        $out[0] =
+            array_search(strtoupper(trim($e[0])), self::CLASSES)
+                ? strtoupper(trim($e[0]))
+                : null;
+        $out[1] =
+            isset($e[1]) && in_array($e[1], [2,10])
+                ? $e[1]
+                : null;
+        return $out;
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param string $name
+     * @param integer $angle
+     * @param string $baseFrom
+     * @return array|false
+     */
+    private static function _retrieveConst($name, $angle, string $baseFrom = "\\tei187\\ColorTools\\StandardIlluminants\\WhitePoint") {
+        $check = self::_checkString($name);
+        if($check[0] === null) {
+            $check = null;
+        } else { 
+            $name = $check[0];
+            $angle = 
+                $check[1] === null
+                    ? ( in_array($angle, [2,10]) ? $angle : 2 )
+                    : $check[1];
+            $check = constant($baseFrom.$angle."::".strtoupper(trim($name)));
+        }
+
+        return is_null($check) ? false : $check;
+    }
+
+    /**
+     * Attempts to retrieve specific chromatic coordinates of white point, per arguments used.
+     *
+     * @param string $name 
+     * @param integer $angle
+     * @return array|false
+     */
+    public static function getChromaticCoordinates(string $name, int $angle = 2) {
+        return self::_retrieveConst($name, $angle);
+    }
+
+    /**
+     * Attempts to retrieve specific chromatic coordinates of white point, per arguments used.
+     *
+     * @param string $name 
+     * @param integer $angle
+     * @return array|false
+     */
+    public static function getTristimulus(string $name, int $angle = 2) {
+        $xy = self::_retrieveConst($name, $angle);
+        if($xy !== false) {
+            return Convert::chromaticity_to_tristimulus($xy);
+        }
+        return false;
     }
 }
