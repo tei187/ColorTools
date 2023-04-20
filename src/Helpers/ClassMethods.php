28221286bb6fbe6749f9efa89a616d6e912183d7
tei187 <59267193+tei187@users.noreply.github.com>
2023-04-20 21:20:04 +0200

    ClassMethods static methods added

---
+++ src/Helpers/ClassMethods.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace tei187\ColorTools\Helpers;
+
+class ClassMethods {
+    /**
+     * Verifies if listed methods are available in class.
+     *
+     * @param array|string $needle
+     * @param object $obj
+     * @return boolean
+     */
+    static public function checkMethods($needle, object $obj) : bool {
+        $methods = get_class_methods($obj);
+        $outcome = false;
+
+        switch(gettype($needle)) {
+            case "string":
+                $outcome = in_array(trim($needle), $methods) !== false ? true : false;
+                break;
+            case "array":
+                $tests = [];
+                foreach($needle as $item) {
+                    $tests[] = in_array(trim($item), $methods) !== false ? true : false;
+                }
+                $outcome = in_array(false, $tests) !== false ? false : true;
+                break;
+        }
+        return $outcome;
+    }
+
+    static function checkForInterface($obj, string $interface) : bool {
+        return in_array($interface, self::get_all_interfaces($obj));
+    }
+
+    static function checkForTrait($obj, string $trait) : bool {
+        return in_array($trait, self::get_all_traits($obj));
+    }
+
+    /**
+     * Returns array of all inherited and current traits.
+     *
+     * @param object|string $obj
+     * @return array
+     */
+    static function get_all_traits($obj) : array {
+        $parentClasses = class_parents($obj);
+        $traits = class_uses($obj);       
+        foreach ($parentClasses as $parentClass) {
+            $traits = array_merge($traits, class_uses($parentClass));
+        }
+        return $traits;
+    }
+
+    /**
+     * Returns array of all inherited and current interfaces.
+     *
+     * @param object|string $obj
+     * @return array
+     */
+    static function get_all_interfaces($obj) : array {
+        $parentClasses = class_parents($obj);
+        $interfaces = class_implements($obj);       
+        foreach ($parentClasses as $parentClass) {
+            $interfaces = array_merge($interfaces, class_implements($parentClass));
+        }
+        return $interfaces;
+    }
+}
